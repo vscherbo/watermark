c25db1be616bef7ef3d5da57e8c762c6cd22c0ac
Vladimir Scherbo <vscherbo@gmail.com>
2022-07-11 23:18:41 +0300

    semi-transparent font; horizontal ok; vertical is not

--- wm.sh
+++ wm.sh
@@ -10,69 +10,28 @@ WMARK=$(date +'%F ЩВА')
 
 rm -f $DST
 
-#            +distort SRT '4 270 ' \
-# not so good          \( -fill red -size 390x300 -background transparent label:"$WMARK" \
-
-#convert IMG_2881-all-progressive-max90.jpg -format "%wx%h\n" info:
-
 width=$(convert $SRC -format "%w" info:)
 height=$(convert $SRC -format "%h" info:)
 
-#color='rgb(220,220,220,1.0)'
+color='graya(50%,0.0)'
+#color='rgb(220,220,220,0.0)'
 #color='rgb(220,220,220)'
-color='red'
+#color='red'
 
-# эмпирика: ширина надписи - 1/5, высота надписи - 1/20
-label_w=$(($width/5))
-label_h=$(($height/20))
-echo label_w=${label_w}
-echo label_h=${label_h}
 
 if [ $width -ge $height ]
 then
 	mode=h  # horizontal
 	angle='0'
-	label_size=${label_w}x${label_h}
-	#label_size=${label_w}x
-	w1=$((${width}-${label_w}))
-	h1=$((${height}-${label_h}))
 	kegl=$((width*3/4/25))  # 3/4 - point to pixel factor; 25 - empiric part of width
 else
 	mode=v  # vertical
 	angle='270'
-	#label_size=x${label_h}
-	label_size=${label_w}x${label_w}
-	# при повороте label_h и label_w меняются местами
-	# при задании label_size
-	w1=$((${width}-${label_h}))
-	h1=$((${height}-${label_w}))
-	#w1=$((${width}-${label_h}+15))
-	#h1=$((${height}-${label_w}-2))
-
-	kegl=$((height*3/4/25))  # 3/4 - point to pixel factor; 25 - empiric part of width
-	# при задании kegl
-	w1=$((${width}*3/4))  # W-W*1/25
-	h1=$((${height}-20))  # just test
-
-	# исходя из фактического размера файла с подписью для 100012863_2.png
-	#label_w=186
-	#label_h=34
-	#w1=$((${width}-${label_w}))
-	#h1=$((${height}-${label_h}))
-
+	kegl=$((height*3/4/20))  # 3/4 - point to pixel factor; 25 - empiric part of height
 fi
-
-
-
-# watermark top-left corner need
-# head_image_B.png 624x324
-
-
-#          \( -fill $color -pointsize $kegl -background transparent label:"$WMARK" \
 
 # make label image
 
-#        -size $label_size \
 # AR-1H21-GBN.png  -size 133x133 \
 WM_DST="$(namename $1)"_wm"$src_ext"
 set -vx
@@ -86,8 +45,38 @@ set +vx
 convert "$WM_DST" -format "======== %wx%h\n" info:
 label_w=$(convert $WM_DST -format "%w" info:)
 label_h=$(convert $WM_DST -format "%h" info:)
-w1=$((${width}-${label_w}))
-h1=$((${height}-${label_h}))
+echo label_w=$label_w
+echo label_h=$label_h
+
+
+
+
+if [ $width -ge $height ]
+then
+	w1=$((${width}-${label_w}))
+	h1=$((${height}-${label_h}))
+else
+	set -vx
+	convert -fill $color -stroke 'black' -strokewidth 1 -background transparent \
+			+distort SRT $angle \
+			-pointsize $kegl \
+			-size ${label_h}x${label_h} \
+			label:"$WMARK" \
+			-flatten "$WM_DST"
+	set +vx
+	label_w=$(convert $WM_DST -format "%w" info:)
+	label_h=$(convert $WM_DST -format "%h" info:)
+	echo label_w=$label_w
+	echo label_h=$label_h
+	shift=0
+	w1=$((${width}-$shift-${label_w}/2))
+	h1=$((${height}-$shift-${label_h}/2))
+	echo 'calculated w1='$w1
+	echo 'calculated h1='$h1
+	# AR-1H21-GBN.png OK
+	#w1=390
+	#h1=490
+fi
 # AR-1H21-GBN.png
 #w1=$((${width}-${label_w}/2-10))
 #h1=$((${height}-${label_h}/2-10))
@@ -98,14 +87,11 @@ h1=$((${height}-${label_h}))
 #w1=460 - unvisible
 #h1=590
 
-#w1=400 OK
-#h1=500
 
 echo mode=$mode
 echo width=$width', w1='$w1
 echo height=$height', h1='$h1
 echo 'kegl='$kegl
-#echo 'label_size='$label_size
 # production
 set -vx
 convert "$SRC" \
@@ -124,10 +110,3 @@ set +vx
 
 display "$DST" &
 
-exit 0
-
-convert "$SRC" \
-		  -set option:my:right_edge '0,%[fx:h/1.15]' \
-          \( -fill red -background transparent label:"$WMARK" \
-             +distort SRT '%[fx:w-40],%[fx:h-810] 4 270 %[my:right_edge]' \
-          \) -flatten "$DST"
