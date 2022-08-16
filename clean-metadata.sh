5d9fbe2a3750818de6c4c1046b11a2098938c179
Vladimir Scherbo <vscherbo@gmail.com>
2022-08-16 17:51:38 +0300

    dbl quotes and shellchecks

--- clean-metadata.sh
+++ clean-metadata.sh
@@ -18,29 +18,26 @@ TESTMODE=0	# if 1 - debug (commands not execute, only echo)
 WORK_DIR=$(dirname "$0")
 FLAG="$WORK_DIR/upload.flag"
 
-STAT=/usr/bin/stat
-SED=/bin/sed
-CHOWN=/bin/chown
-CHMOD=/bin/chmod
 MOGRIFY=/usr/bin/mogrify
 EXIFTOOL=/usr/bin/exiftool
 PDFTK=/usr/bin/pdftk
 QPDF=/usr/bin/qpdf
 
-LOGFILE="$WORK_DIR/$(namename $0).log"
+LOGFILE="$WORK_DIR"/$(namename "$0").log
 
 
 
 CleanMeta () {
+  logmsg INFO "checking $1"
 
-  EXT=`ext $1`
+  EXT=$(ext "$1")
   case $EXT in
    .jpg|.JPG|.jpeg|.JPEG|.png|.PNG)
 		DT=$($EXIFTOOL	-csv -csvDelim '^' -DateTimeOriginal "$1" |tail -1)
 		echo "$DT" |grep -q '\^' 
 		if [ $? -eq 0 ]
 		then  # found '^', save DT
-			DT=$(echo $DT|sed 's/.*\^//g')
+			DT=$(echo "$DT"|sed 's/.*\^//g')
 		else
 			DT=
 		fi
@@ -57,25 +54,40 @@ CleanMeta () {
    ;;
    .pdf|.PDF)
 
-       $ECHO $QPDF --decrypt $1 $1-decrypted
+       $ECHO $QPDF --decrypt "$1" "$1-decrypted"
        RC=$?
-       [ $RC -eq 0 ] && $ECHO mv $1-decrypted $1 || logmsg $RC "Failed qpdf on file $1 with rc=$RC" 
+       if [ $RC -eq 0 ]
+       then
+           $ECHO mv "$1-decrypted" "$1"
+       else
+           logmsg $RC "Failed qpdf on file $1 with rc=$RC" 
+       fi
 
-       $ECHO $EXIFTOOL -XMP:All= $1
+       $ECHO $EXIFTOOL -XMP:All= "$1"
        RC=$?
-       [ $RC -eq 0 ] || logmsg $RC "Failed exiftool on file $1 with rc=$RC" && { [ -f $1_original ] && rm $1_original; }
+       if [ $RC -eq 0 ]
+       then
+           [ -f "$1_original" ] && rm "$1_original"
+       else
+           logmsg $RC "Failed exiftool on file $1 with rc=$RC"
+       fi
 
-       $ECHO $PDFTK $1 update_info_utf8 "$WORK_DIR/metautf8.txt" output $1-clean
+       $ECHO $PDFTK "$1" update_info_utf8 "$WORK_DIR/metautf8.txt" output "$1-clean"
        RC=$?
-       [ $RC -eq 0 ] && $ECHO mv $1-clean $1 || logmsg $RC "Failed pdftk on file $1 with rc=$RC"
+       if [ $RC -eq 0 ]
+       then
+           $ECHO mv "$1-clean" "$1"
+       else
+           logmsg $RC "Failed pdftk on file $1 with rc=$RC"
+       fi
    ;;
    *) logmsg WARNING "Wrong argument=$1"
   esac
 }
 
-cd $WORK_DIR
+cd "$WORK_DIR"
 
-exec 1>>$LOGFILE  2>&1
+exec 1>>"$LOGFILE"  2>&1
 
 if [ $TESTMODE -eq 1 ]; then
 	ECHO=/bin/echo
@@ -85,19 +97,17 @@ fi
 
 logmsg INFO "Started"
 
-IFS_SAVE=$IFS
-IFS=";"
+#IFS_SAVE=$IFS
+#IFS=";"
+#for f in $(find $UPLOAD_DIR \( -path "*/rekvizit/*" -prune \) -o \( -path "*/resize_cache/*" -prune \) -o \( -path "*/tmp/*" -prune \) -o -newermm "$FLAG" -type f -regextype posix-egrep \( -iregex '.*\.jp(e|)g$' -o -iregex '.*\.png$' -o -iname '*.pdf' \) -exec printf "%s;" {} \;)
+#do
+#  CleanMeta "$f"
+#done
+#IFS=$IFS_SAVE
 
-for f in `find $UPLOAD_DIR \( -path "*/rekvizit/*" -prune \) -o \( -path "*/resize_cache/*" -prune \) -o \( -path "*/tmp/*" -prune \) -o -newermm "$FLAG" -type f -regextype posix-egrep \( -iregex '.*\.jp(e|)g$' -o -iregex '.*\.png$' -o -iname '*.pdf' \) -exec printf "%s;" {} \;`
-do
-  logmsg INFO "checking $f"
-  #F_OWN=`$STAT --format '%U:%G' "$f"` 
-  #F_PERM=`$STAT --format '%a' "$f" `
-  CleanMeta "$f"
-  #$ECHO $CHOWN ${F_OWN} ${f}
-  #$ECHO $CHMOD ${F_PERM} ${f}
-done
-IFS=$IFS_SAVE
+find $UPLOAD_DIR \( -path "*/rekvizit/*" -prune \) -o \( -path "*/resize_cache/*" -prune \) -o \( -path "*/tmp/*" -prune \) -o -newermm "$FLAG" -type f -regextype posix-egrep \( -iregex '.*\.jp(e|)g$' -o -iregex '.*\.png$' -o -iname '*.pdf' \) -exec printf "%s;" {} \; | while IFS= read -r -d '' file; do
+  CleanMeta "$file"
+done    
 
 $ECHO touch "$FLAG"
 
